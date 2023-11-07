8afb5452dc7e37454551bbc4179f22bbde398df1
bdmg114 <65209655+bdmg114@users.noreply.github.com>
2023-11-08 00:54:11 +0900

    fff

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import speech_to_text
 import kiosk_backend
 import text_to_speech
 from tkinter import *
+from tkinter import messagebox
 import customtkinter
 import os
+import time
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -16,7 +18,8 @@ class App(customtkinter.CTk):
         }
 
         self.title("my app")
-        self.geometry('800x480')
+        #self.attributes("-fullscreen", True)
+        self.geometry('800x480+0+0')
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
         
@@ -45,14 +48,15 @@ class App(customtkinter.CTk):
         self.sum_quant = customtkinter.CTkLabel(self.orders_frame, text=str(self.orders['quantities'][2]))
         self.sum_quant.grid(row=2, column=1, padx=10, pady=(10, 0), sticky="ew")
 
+        self.AIbutton = customtkinter.CTkButton(self, text="Order with AI", command=self.AI_Button)
+        self.AIbutton.grid(row=3, column=0, padx=10, pady=5, sticky="ew", columnspan=2)
         self.button = customtkinter.CTkButton(self, text="Order", command=self.button_callback)
-        self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew", columnspan=2)
+        self.button.grid(row=4, column=0, padx=10, pady=10, sticky="ew", columnspan=2)
+
     def order(self,menu):
         self.orders['quantities'][menu] += 1
-        self.sum_quant.configure(text = str(self.orders['quantities'][2]))
-        self.salad_quant.configure(text = str(self.orders['quantities'][1]))
-        self.shake_quant.configure(text = str(self.orders['quantities'][0]))
-        
+        self.set_quatity_text()
+    
     def button_callback(self):
         print(self.orders)
         self.orders = {
@@ -60,12 +64,84 @@ class App(customtkinter.CTk):
             "quantities": [0,0,0],
             "extra_info": ['','','']
         }
+
+        main_window_width = self.winfo_width()
+        main_window_height = self.winfo_height()
+
+        # Calculate the position to center the modal dialog
+        modal_width = int(main_window_width/3) # Width of the modal dialog
+        modal_height = int(main_window_height/3)  # Height of the modal dialog
+        x_position = (main_window_width - modal_width) // 2
+        y_position = (main_window_height - modal_height) // 2
+
+        self.set_quatity_text()
+        modal_dialog = Toplevel(self)
+        modal_dialog.title("")
+        modal_dialog.overrideredirect(1)  # Remove window decorations (including title bar)
+        modal_dialog.geometry(f"{modal_width}x{modal_height}+{x_position}+{y_position}")
+        modal_dialog.grab_set()  # Grab focus and block interactions with other windows
+        
+        modal_dialog.grid_columnconfigure(0, weight=1)
+        modal_dialog.grid_rowconfigure(0, weight=1)
+
+        # Add a label to the modal dialog
+        processing_label = customtkinter.CTkLabel(modal_dialog, text="Order completed")
+        processing_label.grid(row=0, column=0, padx=20, pady=20, sticky="ew")
+
+        self.update()
+
+        time.sleep(2)
+
+        modal_dialog.destroy()
+
+    def set_quatity_text(self):
         self.sum_quant.configure(text = str(self.orders['quantities'][2]))
         self.salad_quant.configure(text = str(self.orders['quantities'][1]))
         self.shake_quant.configure(text = str(self.orders['quantities'][0]))
 
+    def AI_Button(self):
+        main_window_width = self.winfo_width()
+        main_window_height = self.winfo_height()
 
+        # Calculate the position to center the modal dialog
+        modal_width = int(main_window_width/3) # Width of the modal dialog
+        modal_height = int(main_window_height/3)  # Height of the modal dialog
+        x_position = (main_window_width - modal_width) // 2
+        y_position = (main_window_height - modal_height) // 2
 
+        # Create a modal dialog window without a title bar and center it
+        modal_dialog = Toplevel(self)
+        modal_dialog.title("")
+        modal_dialog.overrideredirect(1)  # Remove window decorations (including title bar)
+        modal_dialog.geometry(f"{modal_width}x{modal_height}+{x_position}+{y_position}")
+        modal_dialog.grab_set()  # Grab focus and block interactions with other windows
+
+        # Add a label to the modal dialog
+        processing_label = Label(modal_dialog, text="Getting ready...")
+        processing_label.pack(padx=20, pady=20)
+
+        self.update()
+
+        try:
+            # Call the backend processing functions
+            s = kiosk_backend.start()
+            if s != '!P':
+                q = 'k'
+                while q != '':
+                    if text_to_speech.gtts_test(s):
+                        q = speech_to_text.start_speech_recognition()
+                        s = kiosk_backend.get_input(q)
+                out = kiosk_backend.output()
+                for x in range(len(out['quantities'])):
+                    self.orders['quantities'][self.orders['quantities'].index(out['menu_items'][x])] += out['quantities'][x]
+                self.set_quantity_text()
+        except Exception as e:
+            self.after(0, lambda: self.show_error_message(str(e)))
+        # Close the modal dialog after processing or displaying error
+        finally:
+            modal_dialog.destroy()
+    def show_error_message(self, error_message):
+        messagebox.showerror("Error", error_message)
 app = App()
 app.mainloop()
 # s = kiosk_backend.start()
--- speech_to_text.py
+++ speech_to_text.py
@@ -1,8 +1,10 @@
 import speech_recognition as sr
+import threading
+import pygame
 
 print(sr.__version__)
 
-def listen():
+def get_ready_to_listen():
     r = sr.Recognizer()
 
     names = sr.Microphone.list_microphone_names()
@@ -11,8 +13,30 @@ def listen():
     mic = sr.Microphone(device_index = 2)
     with mic as source:
         r.adjust_for_ambient_noise(source)
-        print("listening...")
-        audio = r.listen(source)
+        return True
 
-    result = r.recognize_google(audio, language = "en-US")
-    return result
+# Create an event to signal the completion of speech recognition
+speech_recognition_complete = threading.Event()
+def speech_recognition_thread():
+    r = sr.Recognizer()
+    mic = sr.Microphone(device_index=2)
+    with mic as source:
+        r.adjust_for_ambient_noise(source)
+        ding = pygame.mixer.Sound("ding.mp3")
+        ding.play()
+        audio = r.listen(source)
+    result = r.recognize_google(audio, language="en-US")
+    # Process the recognized speech result
+    speech_recognition_result = result
+    # Set the event to signal completion
+    speech_recognition_complete.set()
+
+def start_speech_recognition():
+    global speech_recognition_result
+    speech_recognition_result = None  # Reset result variable
+    speech_recognition_complete.clear()  # Clear the event flag
+    speech_thread = threading.Thread(target=speech_recognition_thread)
+    speech_thread.start()
+    # Wait for the event to be set (speech recognition completion)
+    speech_recognition_complete.wait()
+    return speech_recognition_result

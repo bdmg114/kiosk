826c440af35ac7729777e74fc536b0b275bffbe6
bdmg114 <65209655+bdmg114@users.noreply.github.com>
2023-11-08 00:54:00 +0900

    vvvv

--- kiosk_backend.py
+++ kiosk_backend.py
@@ -6,63 +6,68 @@ import speech_to_text
 
 openai.organization = "org-CRggXsSZlMOf2ystqQcgBWC2"
 openai.api_key = os.environ['OPENAI_API_KEY']
+API_key = os.environ['OPENAI_API_KEY']
 openai.Model.list()
 
 messages =[
-    {
-        "role": "system",
-        "content": '''
-            say only "Y" if you understood. when I say "order" reset what you have done with the last customer and Act as worker at fast food restaurent. you can only accept order that are in the menu below:{1. milsut shake swamp, 2. mil worm forest salads, 3. dim island} when customer arrives, you have to introduce the menu. if customer look for something that is not on the menu suggest alternative option on the menu. when you talk to the customer, when the customer finishes the order, confirm the customer that is he really finished. and if you are sure it is finished, say "thank you". when I say "gfhj", put <S> in the first line of the output and fill in this format and print without anything else to send order data to server. 
-            data format:
-
-            `menu_items`: List of menu items (strings)
-            `quantities`: List of corresponding quantities (integers)
-            `extra_info`: List of additional information (strings)
-
-            Example:
-            menu_items = ["Milsut Shake Swamp", "Dim Island"]
-            quantities = [3, 1]
-            extra_info = ["-", "-"]
-            '''
-    }
 ]
 respone = ''
 def start():
-    response = requests.post(
-        "https://api.openai.com/v1/chat/completions",
-        headers={"Authorization": f"Bearer {API_key}"},
-        json={"model": "gpt-3.5-turbo", "messages": messages},
-    )
+    messages =[
+        {
+            "role": "system",
+            "content": '''
+                say only "Y" if you understood. when I say "order" reset what you have done with the last customer and Act as worker at fast food restaurent. you can only accept order that are in the menu below:{1. milsut shake swamp, 2. mil worm forest salads, 3. dim island} when customer arrives, you have to introduce the menu. if customer look for something that is not on the menu suggest alternative option on the menu. when you talk to the customer, when the customer finishes the order, confirm the customer that is he really finished. and if you are sure it is finished, say "thank you". when I say "gfhj", put <S> in the first line of the output and fill in this format and print without anything else to send order data to server. 
+                data format:
 
-    if response.json()["choices"][0]["message"]["content"] == "Y":
-        messages.extend([
-            {
-                "role": "assistant",
-                "content":str(response.json()["choices"][0]["message"]["content"])
-            },
-            {
-                "role": "system",
-                "content": 'order'
-            },    
-            {
-                "role":"user",
-                "content": "hi!"
-            }])
-        print(messages)
-        response = requests.post(
-        "https://api.openai.com/v1/chat/completions",
-        headers={"Authorization": f"Bearer {API_key}"},
-        json={"model": "gpt-3.5-turbo", "messages": messages},
+                `menu_items`: List of menu items (strings)
+                `quantities`: List of corresponding quantities (integers)
+                `extra_info`: List of additional information (strings)
+
+                Example:
+                menu_items = ["Milsut Shake Swamp", "Dim Island"]
+                quantities = [3, 1]
+                extra_info = ["-", "-"]
+                '''
+        }
+    ]
+    response = requests.post(
+            "https://api.openai.com/v1/chat/completions",
+            headers={"Authorization": f"Bearer {API_key}"},
+            json={"model": "gpt-3.5-turbo", "messages": messages},
         )
-        messages.append(
-            {
+    while response.json()["choices"][0]["message"]["content"] != "Y":
+        response = requests.post(
+            "https://api.openai.com/v1/chat/completions",
+            headers={"Authorization": f"Bearer {API_key}"},
+            json={"model": "gpt-3.5-turbo", "messages": messages},
+        )
+    messages.extend([
+        {
             "role": "assistant",
             "content":str(response.json()["choices"][0]["message"]["content"])
-            }
-        )
-        return response.json()["choices"][0]["message"]["content"]
-    else:
-        return "!P"
+        },
+        {
+            "role": "system",
+            "content": 'order'
+        },    
+        {
+            "role":"user",
+            "content": "hi!"
+        }])
+    print(messages)
+    response = requests.post(
+    "https://api.openai.com/v1/chat/completions",
+    headers={"Authorization": f"Bearer {API_key}"},
+    json={"model": "gpt-3.5-turbo", "messages": messages},
+    )
+    messages.append(
+        {
+        "role": "assistant",
+        "content":str(response.json()["choices"][0]["message"]["content"])
+        }
+    )
+    return response.json()["choices"][0]["message"]["content"]
 
 def get_input(k):
     a = k

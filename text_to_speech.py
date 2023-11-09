05f02133186e9df4a6184235c3d9de9999a76604
bdmg114 <65209655+bdmg114@users.noreply.github.com>
2023-11-10 03:05:12 +0900

    sfsds

--- text_to_speech.py
+++ text_to_speech.py
@@ -2,6 +2,7 @@ from gtts import gTTS
 import os
 import pygame
 import time
+from threading import Timer
 
 
 def gtts_test(text: str):
@@ -10,9 +11,11 @@ def gtts_test(text: str):
     tts.save(f"{fn[0:8]}.mp3")
     saying = pygame.mixer.Sound(f"{fn[0:8]}.mp3")
     saying.play()
-    while pygame.mixer.music.get_busy():
-        pygame.time.Clock().tick(10)  # Adjust the value inside tick() to control the speed of the loop
+    t = Timer(saying.get_length(), fin(fn))
+    pygame.mixer.music.stop()  # Adjust the value inside tick() to control the speed of the loop
+    return t
     
-    pygame.mixer.music.stop()
+
+def fin(fn):
     os.remove(f"{fn[:8]}.mp3") 
     return True
